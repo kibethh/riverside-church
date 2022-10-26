175093610a21c8afe167131dbcb1bf7457aeb977
kibethh <kibethh@gmail.com>
2022-10-26 10:40:05 +0300

    updated Mpesa

--- src/controllers/paymentController.js
+++ src/controllers/paymentController.js
@@ -2,6 +2,7 @@ const stripe = require('stripe');
 const prettyjson = require('prettyjson');
 const stringify = require('json-stable-stringify');
 const axios = require('axios');
+const fetch = require('node-fetch');
 const Payment = require('../models/payment');
 const catchAsync = require('../utils/catchAsync');
 const APIFeatures = require('../utils/apiFeatures');
@@ -24,18 +25,20 @@ exports.getOAuthToken = catchAsync(async (req, res, next) => {
 
   //form a buffer of the consumer key and secret
   let buffer = new Buffer.from(consumer_key + ':' + consumer_secret);
+  const parsedBuffer = buffer.toString('base64');
 
-  let auth = `Basic ${buffer.toString('base64')}`;
+  let auth = `Basic ${parsedBuffer.trim()}`;
 
-  let { data } = await axios.get(
+  const { data } = await axios.get(
     'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials',
     {
       headers: {
+        Accept: 'application/json',
         Authorization: auth,
       },
     }
   );
-  // console.log(data);
+  console.log(data);
   // asigning access token to a variable in the next middleware
   req.data = req.query;
   req.token = data['access_token'];
@@ -58,30 +61,54 @@ exports.lipaNaMpesaOnline = catchAsync(async (req, res) => {
     today.getFullYear() +
     ('0' + (today.getMonth() + 1)).slice(-2) +
     ('0' + today.getDate()).slice(-2);
-  const time =
-    today.getHours().toString() +
-    today.getMinutes().toString() +
-    today.getSeconds().toString();
+
+  const hours =
+    today.getHours().toString().length == 2
+      ? today.getHours().toString()
+      : 0 + today.getHours().toString();
+  const minutes =
+    today.getMinutes().toString().length == 2
+      ? today.getMinutes().toString()
+      : 0 + today.getMinutes().toString();
+  const seconds =
+    today.getSeconds().toString().length == 2
+      ? today.getSeconds().toString()
+      : 0 + today.getSeconds().toString();
+
+  const time = hours + minutes + seconds;
+
   const Timestamp = date + time;
 
+  const BusinessShortCode = parseInt(process.env.BusinessShortCode);
   let buffer = new Buffer.from(
-    process.env.BusinessShortCode + process.env.PassKey + Timestamp
+    BusinessShortCode + process.env.PassKey.trim() + Timestamp.trim()
   );
 
-  let Password = buffer.toString('base64');
+  let Password = buffer.toString('base64').split('=')[0];
+  // let Password =
+  //   'MTc0Mzc5YmZiMjc5ZjlhYTliZGJjZjE1OGU5N2RkNzFhNDY3Y2QyZTBjODkzMDU5YjEwZjc4ZTZiNzJhZGExZWQyYzkxOTIwMjIxMDI1MTYwOTE3';
+
+  console.log('pass', Password.toString());
+
+  const TransactionType = process.env.TransactionType.trim();
+  const AccountReference = process.env.AccountReference.trim();
+  const TransactionDesc = process.env.TransactionDesc.trim();
+  const PartyA = parseInt(process.env.MPESA_PARTY_A);
+  const PartyB = parseInt(process.env.MPESA_PARTY_B);
+  const CallBackURL = process.env.CallBackURL.trim();
 
   const payLoad = {
-    BusinessShortCode: 174379,
+    BusinessShortCode,
     Password,
     Timestamp,
-    TransactionType: 'CustomerPayBillOnline',
+    TransactionType,
     Amount,
-    PartyA: PhoneNumber,
-    PartyB: 174379,
+    PartyA,
+    PartyB,
     PhoneNumber,
-    CallBackURL: process.env.CallBackURL + '/api/v1/payment/mpesastatus',
-    AccountReference: 'test',
-    TransactionDesc: 'test',
+    CallBackURL,
+    AccountReference,
+    TransactionDesc,
   };
 
   let { data } = await axios.post(
